078840308ab3586eff7f332b0554826f9e6e103d
Ezequiel Calderara <ecalderara@medallia.com>
2020-11-04 10:46:05 -0300

    added assets and planets rotation

---
+++ src/Planet.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	set_angular_velocity(10);
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+#	rotation += delta;
+	pass
